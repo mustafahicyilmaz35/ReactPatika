3d20619bda28c5381b648884233e50d683e1739b
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2021-04-06 21:00:05 +0300

    Initialize

---
+++ yazi-tura/src/components/CoinFlipper/CoinFlipper.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import Coin from "../Coin/Coin";
+import "./CoinFlipper.css";
+
+class CoinFlipper extends Component {
+  constructor(props) {
+    super(props);
+    // State üzerinde paranın başlangıçtaki durumunu veriyoruz, başlangıçta "tura" olsun.
+    // Daha sonra şu anda paranın dönüp dönmeme durumunu da veriyoruz, başlangıçta para atılmamış olduğundan "false" olarak verdik.
+    this.state = {
+      side: "tura",
+      flipping: false,
+      count:0,
+      yazi:0,
+      tura:0
+    };
+  }
+  handleClick = () => {
+    var random =(Math.random()>=0.5)? 1 : 0;
+    // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
+    this.setState({ flipping: true, count:this.state.count + 1,random:random==0?this.state.tura++ : this.state.yazi++ });
+    // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
+    setTimeout(() => this.setState({ flipping: false }), 1000);
+  };
+
+  render() {
+    
+    return (
+      <div className="CoinFlipper">
+        <h1>Yazı mı Tura mı?</h1>
+        <Coin side={this.state.side} flipping={this.state.flipping} />
+        <button onClick={this.handleClick}>At!</button>
+        <p>
+          Toplam
+          <strong> {this.state.count} </strong>
+          atıştan
+          <strong> {this.state.tura} </strong>ü tura
+          <strong> {this.state.yazi} </strong>
+          si yazı geldi.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default CoinFlipper;
